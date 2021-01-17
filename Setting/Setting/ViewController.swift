f2e6255d7c0a37b0810a5d190c11f1e7a2c80186
Neha <Bisht>
2021-01-17 15:48:44 -0500

    Add a table view Add a search bar with cancel button Add title bar

--- Setting/Setting/ViewController.swift
+++ Setting/Setting/ViewController.swift
@@ -7,15 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    @IBOutlet weak var tableView1: UITableView!
+    @IBOutlet weak var searchBar1: UISearchBar!
+let data = ["Neha Harariya"]
+    let data1 = ["Apple Id,icloud,Media & Purchase"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("ok")
+
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        cell.detailTextLabel?.text = data1[indexPath.row]
+        return cell
+    }
+    @IBAction func cancelButton(_sender: Any){
+        
+    }
+    
 }
 
